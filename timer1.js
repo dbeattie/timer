258cbdf7635773aad768b7aa25305b2ff6e00948
dbeattie <darren@fettle.ca>
2019-10-22 20:17:41 +0000

    created a timed that takes in only positive number values from the console and outputs a 'beep'

---
+++ timer1.js
@@ -0,0 +1,11 @@
+// ALARM CLOCK 
+const data = process.argv
+let number = data.slice(2).filter(function(number){return number > -1});
+const timer = function(number) {
+ for (const num of number) {
+    setTimeout(() => {
+    console.log('beep');  
+    }, num * 1000);
+};
+};
+timer(number);
