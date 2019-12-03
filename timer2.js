29a65e70b8bdfba6b32b5ff1a8111cc5b1f2a2b5
dbeattie <darren@fettle.ca>
2019-12-03 19:23:53 +0000

    Created a server side function that listens to command line inputs and outputs a 'BEEP' when you press b, or sets a timer from 1-9 to beep and can escape with ctrl c on command

--- timer2.js
+++ timer2.js
@@ -1,43 +1,68 @@
-//The user can press 'b' and it should beep right away
-//The user can input any number from 1 to 9 and it should
-//--immediately output "setting timer for x seconds...", and
-//--beep after that number of seconds has passed
-
-//The user can use 'ctrl+c' to exit the program...
-//...at which point the program should say... 
-//"Thanks for using me, ciao!" before terminating
+//WHAT WAS THIS FOR???
+// const readline = require('readline');
+// const ask = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
 //SHOULD CREATE A BEEP: process.stdout.write('\x07');
 
 // ALARM CLOCK VAMPED UP 
-const readline = require('readline');
-const ask = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+
+const stdin = process.stdin;
+// don't worry about these next two lines of setup work.
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+stdin.on('data', (key) => {
+  if (key === '\u0003') {
+    console.log(`Thanks for using me, ciao!`);
+    process.exit();
+  } else if (key === 'b') {
+    console.log(`BEEP`);
+    return process.stdout.write('\x07');
+  } else if (key >= 1 && key <= 9) {
+    console.log(`setting timer for ${key} seconds...`);
+    setTimeout(() => {
+      console.log('BEEP!');
+      return process.stdout.write('\x07');
+    }, key * 1000);
+  } else {
+    process.stdout.write('.');
+  }
 });
 
-//ask.question('Press ctrl^c to quit', (response) => {
-  process.on('exit', (code) => {
-      console.log('Thanks for using me, ciao!');
-  }); 
+//console.log('after callback');
 
-ask.question('Press b to Beep:', (response) => {
-    if (response === 'b') {
-      console.log('beep')
-    } ask.question('Input any number from 1-9 to set timer:', (response) => {
-      if (response >= 1 || response <= 9) {
-        console.log(`Setting timer for ${response} seconds...`);
-        const timer = function(response) {
-          for (const num of response) {
-             setTimeout(() => {
-             console.log('beep');  
-             }, num * 1000);
-         };
-         };
-         timer(response);
-    }
-    });
-});    
+//ask.question('Press ctrl^c to quit', (response) => {
+  // process.on('exit', (code) => {
+  //     console.log('Thanks for using me, ciao!');
+  // }); 
+
+
+
+
+
+// ask.question('Press b to Beep:', (response) => {
+//     if (response === 'b') {
+//       console.log('beep');
+//       console.log('BEEP!');
+//       return process.stdout.write('\x07');
+//     } ask.question('Input any number from 1-9 to set timer:', (response) => {
+//       if (response >= 1 || response <= 9) {
+//         console.log(`Setting timer for ${response} seconds...`);
+//         const timer = function(response) {
+//           for (const num of response) {
+//              setTimeout(() => {
+//               console.log('BEEP!');
+//               return process.stdout.write('\x07');  
+//              }, num * 1000);
+//          };
+//          };
+//          timer(response);
+//     }
+//     });
+// });    
 
 
 // ask.question('Account #', (account) => {
